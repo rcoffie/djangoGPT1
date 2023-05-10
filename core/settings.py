ac508f4d3bc3e421e88c9e40ab26665c6f68c5ad
Robert Coffie <rcoffie22@yahoo.com>
2023-05-10 12:33:32 +0000

    adding static files, creating hompage template

--- core/settings.py
+++ core/settings.py
@@ -117,10 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"  # new
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  # new
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 
 # Default primary key field type
